f0c6e91229810ef68c2ad7504d5a4c54bb90d6d6
Sam <samweldon9393@gmail.com>
2025-01-09 19:52:33 -0500

    add bfsFill

--- binarytree.cpp
+++ binarytree.cpp
@@ -13,9 +13,11 @@ class binaryTree{
         binaryTree *left;
         binaryTree *right;
         void *data;
+        size_t elementSize;
 
-        binaryTree(void *data){
+        binaryTree(void *data, size_t size){
             this->data = data;
+            this->elementSize = size;
             this->left = NULL;
             this->right = NULL;
         }
@@ -26,41 +28,94 @@ class binaryTree{
             right = NULL;
         }
 
-        void setLeft(void *data){
-            left = new binaryTree(data);
+        binaryTree *setLeft(void *data){
+            left = new binaryTree(data, elementSize);
+            return left;
         }
 
-        void setRight(void *data){
-            right = new binaryTree(data);
+        binaryTree *setRight(void *data){
+            right = new binaryTree(data, elementSize);
+            return right;
+        }
+
+        binaryTree *linkLeft(binaryTree *newLeft){
+            this->left = newLeft;
+            return left;
+        }
+
+        binaryTree *linkRight(binaryTree *newRight){
+            this->right = newRight;
+            return right;
+        }
+
+        int dfsPrint(binaryTree *node);
+        void bfsPrint();
+
+        // Fill the tree in depth first order with contents of arr 
+        binaryTree *dfsFill(void *arr){
+            
+            if (!arr){
+                return NULL;
+            }
+        
+            char *it = (char *)arr;
+            this->setLeft(arr);
+            this->setRight(it+elementSize); 
+            
+            // TODO how do i do this lol
+            return this;
+
+        }
+
+        // Fill the tree in breadth first order with contents of arr
+        binaryTree *bfsFill(void *arr, size_t num){
+           
+            queue<binaryTree *> q;
+            q.push(this);
+
+            char *it = (char *)arr;
+
+            binaryTree *cur;
+            int j = 0;
+
+            while (j < num){
+                
+                cur = q.front();
+                q.pop();
+
+                q.push(cur->setLeft(it));
+                it += elementSize;
+                if (++j >= num)
+                    break;
+                q.push(cur->setRight(it));
+                it += elementSize;
+                j++;
+            }
+
+            return this; 
         }
 };
 
-void dfsPrint(binaryTree *node){
+int binaryTree::dfsPrint(binaryTree *node){
 
     if (node == NULL)
-        return;
+        return 0;
     
     printf("%d ", *(int *)node->data);
 
-    dfsPrint(node->left);
-    cout << "\n";
-    dfsPrint(node->right);
+    if (dfsPrint(node->left) == -1)
+       cout << '\n';
+
+    if (dfsPrint(node->right) == 0)
+        cout << '\n';
+    return 1;
 }
 
-void bfsPrintRecur(binaryTree *node){
-    
-    // Print node 
-    printf("%d ", *(int *)node->data);
-
-    // Print both children
-    
-}
-
-void bfsPrint(binaryTree *node){
+void binaryTree::bfsPrint(){
 
     unordered_map<binaryTree *, bool> marked;
     queue<void *> q;
-    q.push(node);
+    q.push(this);
     int level = 1;
 
     //printf("%d ", *(int *)((binaryTree *)q.front())->data);
@@ -87,16 +142,16 @@ void bfsPrint(binaryTree *node){
 
 int main(){
 
-    int x[12] = {0};
-    for (int i = 0 ; i< 12 ; i++){
+    int x[15] = {0};
+    for (int i = 0 ; i< 15 ; i++){
         x[i] = i;
     }
 
-    binaryTree *tree = new binaryTree(x);
+    binaryTree *tree = new binaryTree(x, 4);
 
    
 
-
+/*
     tree->setLeft(x+1);
     tree->setRight(x+2);
 
@@ -113,6 +168,13 @@ int main(){
     tree->left->right->setRight(x+10);
 
     tree->right->left->setLeft(x+11);
+    tree->right->left->setRight(x+12);
+
+    tree->right->right->setLeft(x+13);
+    tree->right->right->setRight(x+14);
+*/
+
+    tree->bfsFill(x+1, 14);
 
     /* Example */ 
     printf("Should look like: \n");
@@ -122,7 +184,7 @@ int main(){
     printf("%d %d\n", *(int *)tree->right->left->data, *(int*) tree->right->right->data);
     printf("%d %d ", *(int *)tree->left->left->left->data, *(int*) tree->left->left->right->data);
     printf("%d %d ", *(int *)tree->left->right->left->data, *(int*) tree->left->right->right->data);
-    printf("%d \n ", *(int *)tree->right->left->left->data);
+    printf("%d %d\n ", *(int *)tree->right->left->left->data, *(int *)tree->right->left->right->data);
     printf("\n\n");
 
     
@@ -133,7 +195,10 @@ int main(){
     
     
     printf("\n\ncalling bfsPrint: \n");
-    bfsPrint(tree);
+    tree->bfsPrint();
+    
+    printf("\n\ncalling dfsPrint: \n");
+    tree->dfsPrint(tree);
     
 }
 
